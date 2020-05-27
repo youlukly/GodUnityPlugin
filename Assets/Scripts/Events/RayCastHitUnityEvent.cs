d40a3c368eb77c1077b29bd3682fc306cbc71d67
youlukly <youbadly@naver.com>
2020-05-27 14:57:51 +0900

    Add events

---
+++ Assets/Scripts/Events/RayCastHitUnityEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+// serializable unity event that returns RayCastHit
+namespace GodUnityPlugin
+{
+    [System.Serializable]
+    public class RayCastHitUnityEvent : UnityEvent<RaycastHit> { }
+}
---
+++ Assets/Scripts/Events/RayCastHitsUnityEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+// serializable unity event that returns RayCastHits
+namespace GodUnityPlugin
+{
+    [System.Serializable]
+    public class RayCastHitsUnityEvent : UnityEvent<RaycastHit[]> { }
+}
