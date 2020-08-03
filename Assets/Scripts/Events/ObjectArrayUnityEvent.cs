c0d8c46e4be299c797758f5111ee607d001ba72c
youlukly <youbadly@naver.com>
2020-08-03 17:49:03 +0900

    Add object events

---
+++ Assets/Scripts/Events/ObjectArrayUnityEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+// serializable unity event that returns array of object
+namespace GodUnityPlugin
+{
+    [System.Serializable]
+    public class ObjectArrayUnityEvent : UnityEvent<object[]> { }
+}
---
+++ Assets/Scripts/Events/ObjectUnityEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+// serializable unity event that returns object
+namespace GodUnityPlugin
+{
+    [System.Serializable]
+    public class ObjectUnityEvent : UnityEvent<object> { }
+}
